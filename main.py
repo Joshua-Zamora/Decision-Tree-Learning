0d0718f2e552d6e00658269c854db8b4eaf191e6
Joshua-Zamora <jazamora6@miners.utep.edu>
2021-05-09 19:37:54 -0600

    Completed Decision Tree learning with base example

--- main.py
+++ main.py
@@ -1,42 +1,49 @@
 import random
-from pprint import pprint
-from turtle import *
 import numpy as np
 
-from draw import draw_tree
+from pprint import pprint
 
-max_values = [2, 2, 2, 2, 3, 3, 2, 2, 4, 4]
+np.seterr(invalid='ignore')
 
 
-def remainder(examples, attribute):
+def entropy(probability):
     total = 0
-    subsets = [[0, 0]] * max_values[attribute]
+    if probability:
+        total += probability * np.log2(probability)
+    if probability != 1:
+        total += (1 - probability) * np.log2(1 - probability)
+
+    return -total
+
+
+def remainder(examples, attribute, max_values):
+    total = 0
+    subsets = np.array([[0, 0]] * max_values[attribute])
 
     for i in range(examples.shape[0]):
-        subsets[examples[i, attribute]][examples[i, -1]] += 1
+        subsets[examples[i, attribute], examples[i, -1]] += 1
 
     for i in range(len(subsets)):
-        positives = subsets[i][1]
-        negatives = subsets[i][0]
-        total += ((positives + negatives) / examples.shape[0]) * (positives / (positives + negatives))
+        positives = subsets[i, 1]
+        negatives = subsets[i, 0]
+        total += ((positives + negatives) / examples.shape[0]) * entropy(positives / (positives + negatives))
 
     return total
 
 
-def gain(length, examples):
+def gain(length, examples, max_values):
     gains = np.zeros(length)
-    entropy = np.count_nonzero(examples[:, -1]) / (examples.shape[0])
+    probability = np.count_nonzero(examples[:, -1]) / examples.shape[0]
+    b = entropy(probability)
 
-    for i in range(gains.shape[0]):
-        gains[i] = abs(entropy - remainder(examples, i))
+    for i in range(length):
+        gains[i] = b - remainder(examples, i, max_values)
 
     return gains
 
 
-def importance(attributes, examples):
-    index = np.argmax(gain(len(attributes), examples))
-
-    return index, attributes[index]
+def importance(attributes, examples, max_values):
+    return np.argmax(gain(len(attributes), examples, max_values))
 
 
 def plurality_value(examples):
@@ -51,7 +58,7 @@ def plurality_value(examples):
         return random.choice(["Yes", "No"])
 
 
-def decision_tree_learning(examples, attributes, parent_examples):
+def decision_tree_learning(examples, attributes, parent_examples, max_values):
     yes_count = np.count_nonzero(examples[:, -1])
     no_count = examples.shape[0] - yes_count
 
@@ -64,14 +71,13 @@ def decision_tree_learning(examples, attributes, parent_examples):
     elif not attributes:
         return plurality_value(examples)
     else:
-        index, a = importance(attributes, examples)
-        attributes.pop(index)
-        tree = [a]
-
-        for i in range(max_values[index]):
+        index = importance(attributes, examples, max_values)
+        tree = [attributes.pop(index)]
+        print(index)
+        for i in range(max_values.pop(index)):
             sub_examples = examples[examples[:, index] == i]
             sub_examples = np.delete(sub_examples, index, 1)
-            sub_tree = decision_tree_learning(sub_examples, attributes, examples)
+            sub_tree = decision_tree_learning(sub_examples, attributes, examples, max_values)
             tree.append(sub_tree)
 
     return tree
@@ -90,20 +96,16 @@ def main():
         [0, 1, 1, 0, 2, 0, 1, 0, 3, 3, 0],
         [1, 1, 1, 1, 2, 2, 0, 1, 1, 1, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
-        [1, 1, 1, 1, 1, 0, 0, 0, 3, 2, 1],
+        [1, 1, 1, 1, 2, 0, 0, 0, 3, 2, 1],
     ])
 
     attributes = ["Alternate", "Bar", "Fri", "Hungry", "Patrons", "Price",
                   "Raining", "Reservation", "Type", "WaitEstimate"]
 
-    tree = decision_tree_learning(examples, attributes, examples)
+    max_values = [2, 2, 2, 2, 3, 3, 2, 2, 4, 4]
+    tree = decision_tree_learning(examples, attributes, examples, max_values)
 
     pprint(tree)
-    '''
-    up()
-    draw_tree(tree, (0, 200), 1)
-    done()
-    '''
 
 
 if __name__ == "__main__":
